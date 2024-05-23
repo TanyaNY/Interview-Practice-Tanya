d0689482d2fc6597f0fa595bc47acbf1ca53e6d4
TanyaNY <tvkalian@gmail.com>
2024-05-22 20:26:00 -0400

    Task #3

--- src/test/java/CTHMS/features/CTHMS Login Scenarios.feature
+++ src/test/java/CTHMS/features/CTHMS Login Scenarios.feature
@@ -5,4 +5,4 @@ Feature: Login Scenarios
     Given a CTHMS user is on the Login Page
     When user enters username "admin@chroma.com" in username text box
     And enters password "123456" in password text box
-    And clicks on Sign In button
+    Then clicks on Sign In button
--- src/test/java/CTHMS/steps/CTHMS_Login_Steps.java
+++ src/test/java/CTHMS/steps/CTHMS_Login_Steps.java
@@ -2,6 +2,7 @@ package CTHMS.steps;
 
 import CTHMS.pages.LoginPage;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 import static org.example.WebDriverUtils.driver;
@@ -17,15 +18,18 @@ public class CTHMS_Login_Steps {
         String url = "https://mexil.it/chroma_tech_academy/hospital/site/login";
         driver.get(url);
     }
+
     @When("user enters username {string} in username text box")
     public void user_enters_username_in_username_text_box(String username) {
         loginPage.userNameTextBox.sendKeys(username);
     }
+
     @When("enters password {string} in password text box")
     public void enters_password_in_password_text_box(String password) {
         loginPage.passwordTextBox.sendKeys(password);
     }
-    @When("clicks on Sign In button")
+
+    @Then("clicks on Sign In button")
     public void clicks_on_sign_in_button() {
         loginPage.signInButton.click();
     }
